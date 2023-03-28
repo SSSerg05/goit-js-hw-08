8c746203a2cf1d1f2df3040a89464564326647f7
SSSerg05 <ssserg05@gmail.com>
2023-03-28 11:41:11 +0300

    inst SimpleLightbox

---
+++ src/js/01-gallery.js
@@ -0,0 +1,36 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+// SimpleLightbox
+// https://simplelightbox.com/
+// https://github.com/andreknieriem/simplelightbox?utm_source=cdnjs&utm_medium=cdnjs_link&utm_campaign=cdnjs_library
+
+const parent = document.querySelector('.gallery');
+
+const cards = galleryItems.map(item => { 
+  const sample = `
+    <li class="gallery__item">
+     <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image"
+          src="${item.preview}" 
+          alt="${item.description}" 
+          title="${item.description}" />
+     </a>
+    </li>`;
+  return sample;
+});
+
+parent.insertAdjacentHTML('afterbegin', cards.join(''));
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	// Do something…
+  gallery.captionDelay = 250;
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e); // Some usefull information
+});
