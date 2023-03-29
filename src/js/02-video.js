4c3dec41d03805f826636f7b09a4f4d888903462
SSSerg05 <ssserg05@gmail.com>
2023-03-29 18:01:01 +0300

    add throttle (not work)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,39 @@
 import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
 
-let playingTime = localStorage.getItem("videoplayer-current-time");
-console.log('ls = ', playingTime);
+/// for work with localStorage
+// Save
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error("Set state error: ", error.message);
+  }
+};
+
+/// for work with localStorage
+// Load
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error("Get state error: ", error.message);
+  }
+};
+
+export default {
+  save,
+  load,
+};
+
+
+
+let playingTime = load("videoplayer-current-time");
 if (!playingTime) { 
   playingTime = 0;
 }
-console.log('ls = ', playingTime);
   
 const iframe = document.querySelector('#vimeo-player')
 const player = new Player(iframe);
@@ -15,7 +43,9 @@ const onGetTime = function(data) {
   player.getCurrentTime().then(function (seconds) {
     // seconds = the current playback position
     console.log(seconds);
-    localStorage.setItem('videoplayer-current-time', seconds);
+
+   save('videoplayer-current-time', seconds);
+
   }).catch(function(error) {
     // an error occurred
   });
@@ -39,6 +69,7 @@ const onSetTime = function(data) {
 };
 
 
-player.on('pause', onGetTime);
-player.on('timeupdate', onGetTime);
+player.on('timeupdate', onGetTime, 1000);
 player.on('play', onSetTime(playingTime));
+
+
